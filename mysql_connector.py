f8ffed90b326751bec9459bd9cdbb81bfc258cd8
Tony Cihak <tony.cihak@phantom.us>
2017-09-07 10:39:08 -0700

    Cleaned up files and changed pip dependencies to pypi

--- mysql_connector.py
+++ mysql_connector.py
@@ -1,6 +1,16 @@
-# -----------------------------------------
-# Phantom sample App Connector python file
-# -----------------------------------------
+# --
+# File: mysql_connector.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2016-2017
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
 
 # Phantom App imports
 import phantom.app as phantom
@@ -9,10 +19,7 @@ from phantom.action_result import ActionResult
 
 # Usage of the consts file is recommended
 # from mysql_consts import *
-import requests
 import json
-from bs4 import BeautifulSoup
-
 import mysql.connector
 
 
@@ -37,113 +44,10 @@ class MysqlConnector(BaseConnector):
         self._my_connection = None
         self._query_record_limit = None
 
-    def _process_empty_reponse(self, response, action_result):
-
-        if response.status_code == 200:
-            return RetVal(phantom.APP_SUCCESS, {})
-
-        return RetVal(action_result.set_status(phantom.APP_ERROR, "Empty response and no information in the header"), None)
-
-    def _process_html_response(self, response, action_result):
-
-        # An html response, treat it like an error
-        status_code = response.status_code
-
-        try:
-            soup = BeautifulSoup(response.text, "html.parser")
-            error_text = soup.text
-            split_lines = error_text.split('\n')
-            split_lines = [x.strip() for x in split_lines if x.strip()]
-            error_text = '\n'.join(split_lines)
-        except:
-            error_text = "Cannot parse error details"
-
-        message = "Status Code: {0}. Data from server:\n{1}\n".format(status_code,
-                error_text)
-
-        message = message.replace('{', '{{').replace('}', '}}')
-
-        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
-
-    def _process_json_response(self, r, action_result):
-
-        # Try a json parse
-        try:
-            resp_json = r.json()
-        except Exception as e:
-            return RetVal(action_result.set_status(phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(str(e))), None)
-
-        # Please specify the status codes here
-        if 200 <= r.status_code < 399:
-            return RetVal(phantom.APP_SUCCESS, resp_json)
-
-        # You should process the error returned in the json
-        message = "Error from server. Status Code: {0} Data from server: {1}".format(
-                r.status_code, r.text.replace('{', '{{').replace('}', '}}'))
-
-        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
-
-    def _process_response(self, r, action_result):
-
-        # store the r_text in debug data, it will get dumped in the logs if the action fails
-        if hasattr(action_result, 'add_debug_data'):
-            action_result.add_debug_data({'r_status_code': r.status_code})
-            action_result.add_debug_data({'r_text': r.text})
-            action_result.add_debug_data({'r_headers': r.headers})
-
-        # Process each 'Content-Type' of response separately
-
-        # Process a json response
-        if 'json' in r.headers.get('Content-Type', ''):
-            return self._process_json_response(r, action_result)
-
-        # Process an HTML resonse, Do this no matter what the api talks.
-        # There is a high chance of a PROXY in between phantom and the rest of
-        # world, in case of errors, PROXY's return HTML, this function parses
-        # the error and adds it to the action_result.
-        if 'html' in r.headers.get('Content-Type', ''):
-            return self._process_html_response(r, action_result)
-
-        # it's not content-type that is to be parsed, handle an empty response
-        if not r.text:
-            return self._process_empty_reponse(r, action_result)
-
-        # everything else is actually an error at this point
-        message = "Can't process response from server. Status Code: {0} Data from server: {1}".format(
-                r.status_code, r.text.replace('{', '{{').replace('}', '}}'))
-
-        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
-
-    def _make_rest_call(self, endpoint, action_result, headers=None, params=None, data=None, method="get"):
-
-        config = self.get_config()
-
-        resp_json = None
-
-        try:
-            request_func = getattr(requests, method)
-        except AttributeError:
-            return RetVal(action_result.set_status(phantom.APP_ERROR, "Invalid method: {0}".format(method)), resp_json)
-
-        # Create a URL to connect to
-        url = self._base_url + endpoint
-
-        try:
-            r = request_func(
-                            url,
-                            auth=("REPLACE ME: Replace with auth credentials"),
-                            json=data,
-                            headers=headers,
-                            verify=config.get('verify_server_cert', False),
-                            params=params)
-        except Exception as e:
-            return RetVal(action_result.set_status( phantom.APP_ERROR, "Error Connecting to server. Details: {0}".format(str(e))), resp_json)
-
-        return self._process_response(r, action_result)
-
     def _handle_test_connectivity(self, param):
 
-        # Add an action result object to self (BaseConnector) to represent the action for this param
+        # Add an action result object to self (BaseConnector)
+        # to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
         self.save_progress("Testing database connection")
@@ -156,13 +60,16 @@ class MysqlConnector(BaseConnector):
             self.save_progress("Test Connectivity Passed")
             return action_result.set_status(phantom.APP_SUCCESS)
         else:
-            return action_result.set_status(phantom.APP_ERROR, "Connection failed")
+            return action_result.set_status(phantom.APP_ERROR,
+                                            "Connection failed")
 
-    def _handle_describe_table(self, param):
+    def _handle_list_columns(self, param):
 
-        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        self.save_progress("In action handler for: {0}".format(
+            self.get_action_identifier()))
 
-        # Add an action result object to self (BaseConnector) to represent the action for this param
+        # Add an action result object to self (BaseConnector)
+        # to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
         my_table = param['table_name']
@@ -178,11 +85,13 @@ class MysqlConnector(BaseConnector):
 
         return action_result.set_status(phantom.APP_SUCCESS)
 
-    def _handle_show_tables(self, param):
+    def _handle_list_tables(self, param):
 
-        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        self.save_progress("In action handler for: {0}".format(
+            self.get_action_identifier()))
 
-        # Add an action result object to self (BaseConnector) to represent the action for this param
+        # Add an action result object to self (BaseConnector)
+        # to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
         query = "SHOW TABLES;"
@@ -199,17 +108,18 @@ class MysqlConnector(BaseConnector):
 
     def _handle_run_query(self, param):
 
-        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        self.save_progress("In action handler for: {0}".format(
+            self.get_action_identifier()))
 
-        # Add an action result object to self (BaseConnector) to represent the action for this param
+        # Add an action result object to self (BaseConnector)
+        # to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
-        my_query = param['query']
+        my_query = param.get('query')
         record_limit = self._query_record_limit
 
-        if 'ignore_limit' in param:
-            if param['ignore_limit'] == 'true':
-                record_limit = None
+        if param.get('ignore_limit') == 'true':
+            record_limit = None
 
         if record_limit:
             my_query = my_query.strip(';') + " limit " + record_limit
@@ -242,11 +152,11 @@ class MysqlConnector(BaseConnector):
         if action_id == 'test_connectivity':
             ret_val = self._handle_test_connectivity(param)
 
-        elif action_id == 'describe_table':
-            ret_val = self._handle_describe_table(param)
+        elif action_id == 'list_columns':
+            ret_val = self._handle_list_columns(param)
 
-        elif action_id == 'show_tables':
-            ret_val = self._handle_show_tables(param)
+        elif action_id == 'list_tables':
+            ret_val = self._handle_list_tables(param)
 
         elif action_id == 'run_query':
             ret_val = self._handle_run_query(param)
