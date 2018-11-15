00001b2baa22bc2ff97cfc3eb61af65df3d8e4d3
hvaghela <hvaghela@splunk.com>
2018-11-15 12:52:07 +0530

    PAPP-5399 Updated __init__.py file with standard format

--- __init__.py
+++ __init__.py
@@ -2,13 +2,4 @@
 # Copyright (c) 2017-2018 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.# --
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# without a valid written license from Splunk Inc. is PROHIBITED.
